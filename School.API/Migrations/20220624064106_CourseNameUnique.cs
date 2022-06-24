4ec9b95b6c0ae6f4dfc4a24ea412522cbb0ed5f0
BrunoSouzza <souza_0010@hotmail.com>
2022-06-24 07:36:50 -0300

    feat: altering entities

---
+++ School.API/Migrations/20220624064106_CourseNameUnique.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.API.Migrations
+{
+    public partial class CourseNameUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Name",
+                table: "Courses",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_Name",
+                table: "Courses");
+        }
+    }
+}
---
+++ School.API/Migrations/20220624073241_StudentNameUnique.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.API.Migrations
+{
+    public partial class StudentNameUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_Name",
+                table: "Students",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Students_Name",
+                table: "Students");
+        }
+    }
+}
